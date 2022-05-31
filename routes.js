5d5e3a9d9294661ab43cedea531310f2e198042b
Raphael <phael92br@gmail.com>
2022-05-31 19:54:21 -0300

    livraria-api/criando mais rotas

--- routes.js
+++ routes.js
@@ -5,4 +5,5 @@ const LivrosController = require('./controllers/LivrosController');
 router.get('/', LivrosController.mostrarTodosOsLivros);
 router.get('/:id', LivrosController.mostrarLivro);
 router.post('/', LivrosController.criar);
+
 module.exports = router;
